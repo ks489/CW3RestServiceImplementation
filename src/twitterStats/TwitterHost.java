7cc082fe1fd3b9b39dad70c0a6aac827024c0c43
ks489 <ks489@student.le.ac.uk>
2017-03-14 09:51:58 +0000

    Added twitter integration

--- src/twitterStats/TwitterHost.java
+++ src/twitterStats/TwitterHost.java
@@ -11,59 +11,48 @@ import twitter4j.auth.AccessToken;
 import java.io.IOException;
 
 public class TwitterHost {
+	
+	private final static String CONSUMER_KEY = "0Jq1pqgM0GNfSkgtMQPyrvadl";
+	private final static String CONSUMER_SECRET = "dRd1hH5nnGpMd4tMZnE6KuIqOdBsbtfPJH2qfRlBPLeILrSgZI";
+	private final static String ACCESS_TOKEN = "61495742-GIbmeqRshY5fCYOQMHVrnOORKc8zLeDMNZkUUvRUg";
+	private final static String ACCESS_TOKEN_SECRET = "9UsiI8xzIrgbZFd35rpw9a1frXFfZlzoq7zqzwxi7VjA0";
+	
 	public static void main(String[] args) throws IOException, TwitterException {
+		
+        try{
+        	TwitterFactory twitterFactory = new TwitterFactory();
 
-        //Type Your Twitter Consumer Key
+            //Create new Twitter instance
 
-       String consumerKey = "...............";
+            Twitter twitter = twitterFactory.getInstance();
 
-        //Type Your Twitter Consumer Secret
+           //setup OAuth Consumer Credentials
 
-       String consumerSecret = ".................";
+            twitter.setOAuthConsumer(CONSUMER_KEY, CONSUMER_SECRET);
 
-        //Type Your Twitter Access Token
+             //setup OAuth Access Token
 
-       String accessToken = "................";
+            twitter.setOAuthAccessToken(new AccessToken(ACCESS_TOKEN, ACCESS_TOKEN_SECRET));
 
-        //Type Your Twitter Access Token Secret
+             String userName = "UoLInformatics";
 
-       String accessTokenSecret = ".....................";
+             //get the most recent 200 tweets from the given user name. 
 
-        //Create thread-safe factory
+             //Note 200 is the maximum number of tweets get per 1 call. 
 
-       TwitterFactory twitterFactory = new TwitterFactory();
+             //check Twitter API documentation for more detail
 
-       //Create new Twitter instance
+             ResponseList<Status> status1 =twitter.getUserTimeline(userName, new Paging(1,200));
 
-       Twitter twitter = twitterFactory.getInstance();
+             // print the tweet text
 
-      //setup OAuth Consumer Credentials
+             for (Status tweets :status1){
 
-       twitter.setOAuthConsumer(consumerKey, consumerSecret);
+                 System.out.println(tweets.getText());
 
-        //setup OAuth Access Token
-
-       twitter.setOAuthAccessToken(new AccessToken(accessToken, accessTokenSecret));
-
-        String userName = "UoLInformatics";
-
-        //get the most recent 200 tweets from the given user name. 
-
-        //Note 200 is the maximum number of tweets get per 1 call. 
-
-        //check Twitter API documentation for more detail
-
-        ResponseList<Status> status1 =twitter.getUserTimeline(userName, new Paging(1,200));
-
-        // print the tweet text
-
-        for (Status tweets :status1){
-
-            System.out.println(tweets.getText());
-
+             }
+        }catch(Exception e){
+        	System.out.println(e.toString());
         }
-
-        
-
   }
 }
